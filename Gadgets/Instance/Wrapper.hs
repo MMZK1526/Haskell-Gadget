ac1a0b460299a7b5e5bb10f8d83e60f4a491a58e
Yitang Chen <yc4120@ic.ac.uk>
2021-05-12 13:28:27 +0800

    Avoid instance overlapping

--- Gadgets/Instance/Wrapper.hs
+++ Gadgets/Instance/Wrapper.hs
@@ -9,13 +9,13 @@ class Wrapper w where
   unwrap :: w a -> a
   wrap   :: a -> w a
 
-instance Wrapper w => Functor w where
+instance {-# OVERLAPPABLE #-} Wrapper w => Functor w where
   fmap = liftM
 
-instance Wrapper w => Applicative w where
+instance {-# OVERLAPPABLE #-} Wrapper w => Applicative w where
   pure  = return
   (<*>) = ap
 
-instance Wrapper w => Monad w where
+instance {-# OVERLAPPABLE #-} Wrapper w => Monad w where
   return = wrap
   (>>=)  = flip (. unwrap)
