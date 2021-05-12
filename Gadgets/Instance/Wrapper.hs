855c6e67146fd4f4d975c92a5f32c72922a0f335
Yitang Chen <yc4120@ic.ac.uk>
2021-05-12 23:16:42 +0800

    Monad Transformer WrapperT

--- Gadgets/Instance/Wrapper.hs
+++ Gadgets/Instance/Wrapper.hs
@@ -23,5 +23,20 @@ instance {-# OVERLAPPABLE #-} Wrapper w => Monad w where
   return = wrap
   (>>=)  = flip (. unwrap)
 
+instance {-# OVERLAPPABLE #-} (Monad m, Wrapper w) 
+  => Functor (WrapperT w m) where
+    fmap = liftM
+
+instance {-# OVERLAPPABLE #-} (Monad m, Wrapper w) 
+  => Applicative (WrapperT w m) where
+    pure  = return
+    (<*>) = ap
+
+instance {-# OVERLAPPABLE #-} (Monad m, Wrapper w) 
+  => Monad (WrapperT w m) where
+    return = WrapperT . return . wrap
+    (WrapperT w) >>= f 
+      = WrapperT $ w >>= runWrapperT . f . unwrap
+
 instance {-# OVERLAPPABLE #-} Wrapper w => MonadTrans (WrapperT w) where
   lift = WrapperT . fmap wrap
