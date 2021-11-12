d70732590cc7a62933bab814d3a63e3a0ab45552
Yitang Chen <yc4120@ic.ac.uk>
2021-11-12 15:22:47 +0000

    Clarification on amortised worst case with mixed :< & tail

--- Gadgets/RAList.hs
+++ Gadgets/RAList.hs
@@ -2,6 +2,8 @@
 
 -- Random Access List with amortised O(1) prepending and O(log n) random
 -- access.
+-- It also provides popping (removing the first element), but mixing pushing &
+-- popping would have O(log n) complexity in the worst case.
 
 module Gadgets.RAList (
   RAList(Empty, (:<)), empty, fromList, head, length, update, update', 
