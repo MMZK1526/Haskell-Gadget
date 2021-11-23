a0cfd433ae51d6b0b87665b1390c0f880dfb731b
Yitang Chen <yc4120@ic.ac.uk>
2021-11-23 10:17:55 +0000

    Implement adjust

--- Gadgets/Array.hs
+++ Gadgets/Array.hs
@@ -1,30 +1,28 @@
 module Gadgets.Array where
 
-import           Data.Array (Array, bounds, listArray, (!), (//))
+import           Control.Monad (ap)
+import           Data.Array (Array, Ix, bounds, listArray, (!), (//))
 
 -- | Making an array from a list, indexed from 0.
 -- 
 fromList :: [a] -> Array Int a
 fromList xs = listArray (0, length xs - 1) xs
 
+-- | Adjusts a value in the array with the given function.
+-- It will do nothing if the index is out of bound.
+adjust :: Ix i => Array i a -> (a -> a) -> i -> Array i a
+adjust arr f i = arr // [(i, f $ arr ! i)]
+
+-- | Strict version of "adjust".
+adjust' :: Ix i => Array i a -> (a -> a) -> i -> Array i a
+adjust' = (. ap seq) . adjust
+
 -- | Safe array access.
 -- 
 infixr 4 !?
-(!?) :: Array Int a -> Int -> Maybe a
+(!?) :: Ix i => Array i a -> i -> Maybe a
 arr !? i 
   | i < inf || i > sup = Nothing
   | otherwise          = Just $ arr ! i
-  where
-     (inf, sup) = bounds arr
-
--- | Update a value in the array.
--- Example: @ arr =: 3 $ 5 @ sets the third element to five.
--- It will produce the same array if the index is out of bound.
--- 
-infixl 3 =:
-(=:) :: Array Int a -> Int -> a -> Array Int a
-(=:) arr i e 
-  | i < inf || i > sup = arr 
-  | otherwise          = arr // [(i, e)]
   where
      (inf, sup) = bounds arr
--- Gadgets/Array/Unboxed.hs
+++ Gadgets/Array/Unboxed.hs
@@ -2,32 +2,29 @@
 
 module Gadgets.Array.Unboxed where
 
+import           Control.Monad (ap)
 import           Data.Array.Unboxed 
-  (IArray, UArray, bounds, listArray, (!), (//))
+  (IArray, Ix, UArray, bounds, listArray, (!), (//))
 
 -- | Making an array from a list, indexed from 0.
 -- 
 fromList :: IArray UArray a => [a] -> UArray Int a
 fromList xs = listArray (0, length xs - 1) xs
 
+-- | Adjusts a value in the array with the given function.
+-- It will do nothing if the index is out of bound.
+adjust :: (Ix i, IArray UArray a) => UArray i a -> (a -> a) -> i -> UArray i a
+adjust arr f i = arr // [(i, f $ arr ! i)]
+
+-- | Strict version of "adjust".
+adjust' :: (Ix i, IArray UArray a) => UArray i a -> (a -> a) -> i -> UArray i a
+adjust' = (. ap seq) . adjust
 -- | Safe array access.
 -- 
 infixr 4 !?
-(!?) :: IArray UArray a => UArray Int a -> Int -> Maybe a
+(!?) :: (Ix i, IArray UArray a) => UArray i a -> i -> Maybe a
 arr !? i 
   | i < inf || i > sup = Nothing
   | otherwise          = Just $ arr ! i
-  where
-     (inf, sup) = bounds arr
-
--- | Update a value in the array.
--- Example: @ arr =: 3 $ 5 @ sets the third element to five.
--- It will produce the same array if the index is out of bound.
--- 
-infixl 3 =:
-(=:) :: IArray UArray a => UArray Int a -> Int -> a -> UArray Int a
-(=:) arr i e 
-  | i < inf || i > sup = arr 
-  | otherwise          = arr // [(i, e)]
   where
      (inf, sup) = bounds arr
