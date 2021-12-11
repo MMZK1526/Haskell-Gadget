2830abbcc234a790ba9bd2319a74228a65f991dc
Yitang Chen <yc4120@ic.ac.uk>
2021-12-11 12:42:03 +0000

    Refactor

--- Gadgets/Maybe.hs
+++ Gadgets/Maybe.hs
@@ -1,7 +1,7 @@
 module Gadgets.Maybe where
 
+import            Control.Monad (guard)
+
 -- | Applies a predicate, returning "Nothing" if False.
 toMaybe :: (a -> Bool) -> a -> Maybe a
-toMaybe f a
-  | f a       = Just a
-  | otherwise = Nothing
+toMaybe f a = guard (f a) >> Just a
