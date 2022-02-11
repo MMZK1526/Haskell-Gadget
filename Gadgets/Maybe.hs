e1278a00754de2f97116d53ae26ad5acdb3a697f
Yitang Chen <yc4120@ic.ac.uk>
2022-02-11 19:23:05 +0000

    More functions for Maybe

--- Gadgets/Maybe.hs
+++ Gadgets/Maybe.hs
@@ -5,3 +5,18 @@ import            Control.Monad (guard)
 -- | Applies a predicate, returning "Nothing" if False.
 toMaybe :: (a -> Bool) -> a -> Maybe a
 toMaybe f a = guard (f a) >> Just a
+{-# INLINE toMaybe #-}
+
+-- | Run the monadic action if the first argument is a "Just".
+onJust :: Applicative m => Maybe a -> (a -> m ()) -> m ()
+onJust ma f = case ma of
+  Just a  -> f a
+  Nothing -> pure ()
+{-# INLINE onJust #-}
+
+-- | Run the monadic action if the first argument is "Nothing".
+onNothing :: Applicative m => Maybe a -> m () -> m ()
+onNothing ma f = case ma of
+  Nothing -> f
+  Just _  -> pure ()
+{-# INLINE onNothing #-}
